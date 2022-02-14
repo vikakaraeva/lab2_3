a389b6df204ae3ecca2889fe204da40733f98df5
Вика Караева <vikakaraevavika@gmail.com>
2022-02-14 09:13:00 +0300

    Добавьте файлы проекта.

---
+++ MySum.exe/MySum.exe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySum.exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ myhello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab2_3
+{
+    class myhello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
